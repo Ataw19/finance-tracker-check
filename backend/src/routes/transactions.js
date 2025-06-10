de2d55e83c44b05b57e9bc3520d3fef3bcc51496
Irhab Muqsith Arrasyid <97715872+BellkunS@users.noreply.github.com>
2025-06-10 23:00:01 +0700

    Update transactions.js

--- backend/src/routes/transactions.js
+++ backend/src/routes/transactions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getTransactions, createTransaction, updateTransaction, deleteTransaction } = require('../controllers/transactionController');
-const { protect } = require('../middleware/authMiddleware');
+const { getTransactions, createTransaction, updateTransaction, deleteTransaction } = require('../Controllers/transactionController.js');
+const { protect } = require('../middleware/auth.js');
 
 router.route('/').get(protect, getTransactions).post(protect, createTransaction);
 router.route('/:id').put(protect, updateTransaction).delete(protect, deleteTransaction);
